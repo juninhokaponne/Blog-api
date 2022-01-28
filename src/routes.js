66c17b417665c2ac6668808cea5e532bd2aee537
GilsonOliveira <juninhokaponnemg@hotmail.com>
2022-01-28 07:54:25 -0300

    identation routes

--- src/routes.js
+++ src/routes.js
@@ -3,24 +3,24 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
 const AuthenticateController = require('./controllers/AuthenticateController');
-
+const Auth = require('./middlewares/Auth');
 
 // Create user on database
-routes.post('/users',UserController.createUser)
+routes.post('/users', UserController.createUser)
 
 // Login
-routes.post('/login',AuthenticateController.login)
+routes.post('/login', AuthenticateController.login)
 
 // List users
-routes.get('/users',UserController.listUsers)
+routes.get('/users',Auth.eAdmin, UserController.listUsers)
 
 // List user by ID
-routes.get('/users/:id',UserController.ListId)
+routes.get('/users/:id',Auth.eAdmin, UserController.ListId)
 
 // Create post
-routes.post('/posts',UserController.createPost)
+routes.post('/posts', UserController.createPost)
 
 //Delete me 
-routes.delete('/users/me', UserController.deleteMe)
+routes.delete('/users/me/:id',Auth.eAdmin, UserController.deleteMe)
 
 module.exports = routes;
