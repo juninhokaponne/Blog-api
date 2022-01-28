debe49a83968ae69de5df6cd288a8fc3d6a29098
GilsonOliveira <juninhokaponnemg@hotmail.com>
2022-01-28 02:24:33 -0300

    added login verification

--- src/controllers/AuthenticateController.js
+++ src/controllers/AuthenticateController.js
@@ -5,8 +5,21 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   async login(req, res) {
+    
+    const { email,password,displayname } = req.body;
+    const user = { name: displayname };
 
-    const { email,password } = req.body;
+    if(!email && !password){
+      return res.status(400).json({
+        message: `"\email\" and "\password\" is required.`
+      })
+    }
+
+    if (!password){ return res.status(400).json({ message: `"\Password\" is required!` });}
+    if (email === "") { return res.status(400).json({ message: `"\email\" is not allowed to be empty` }); }
+    if (password === "") { return res.status(400).json({ message: `"\password\" is not allowed to be empty` });}
+    Isemail.validate("test@iana.org", { errorLevel: true });
+    if (!Isemail.validate(email)) {return res.status(400).json({ message: `"\email\" must be a valid email" ` });}
 
     const userExist = await User.findOne({
       attributes: ['id','email','password'], 
@@ -21,52 +34,17 @@ module.exports = {
       })
     }
 
-    if(!email && !password){
-      return res.status(400).json({
-        message: `"\email\" and "\password\" is required.`
-      })
-    }
+    !email ? res.status(400).json({ message:`"\email\" is required!`}) : false;
 
-    if (!email) {
-      return res.status(400).json({ message: `"\email\" is required!` });
-    }
-    if (!password) {
-      return res.status(400).json({ message: `"\Password\" is required!` });
-    }
-    if (email === "") {
-      return res
-        .status(400)
-        .json({ message: `"\email\" is not allowed to be empty` });
-    }
-    if (password === "") {
-      return res
-        .status(400)
-        .json({ message: `"\password\" is not allowed to be empty` });
-    }
-
-    Isemail.validate("test@iana.org", { errorLevel: true });
-    if (!Isemail.validate(email)) {
-      return res
-        .status(400)
-        .json({ message: `"\email\" must be a valid email" ` });
-    }
-
-    const { displayname } = req.body;
-
-    const user = { name: displayname };
+    if (password !== userExist.password)
+      return res.status(200).json({ message: "User or password is wrong!" });
 
     const accessToken = jwt.sign(user, " " + process.env.ACCES_TOKEN_SECRET);
 
-    res.json({ accessToken: accessToken });
-
-    const users = await User.findAll({
-      attributes: ["email", "password"],
+    res.status(200).json({
+      accessToken: accessToken 
     });
 
-    if (email && password === users) {
-      return res.status(200).json({ message: "Usuário logado com sucesso!" });
-    } else {
-      return res.status(400).json({ message: "Falha ao tentar fazer login!" });
-    }
+
   },
 };
